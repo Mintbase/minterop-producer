b48eef0712cd11b8e4f9fcd5712a6eecf9033d46
Till <till.friesewinkel@googlemail.com>
2022-10-20 15:53:37 +0100

    WIP: not working

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-(
-  cd seed-testnet || exit 1
-  npm ci || exit 1
-  npx ts-node src/index.ts || exit 1
-) || exit 1
+# FIXME: reactivate
+# (
+#   cd seed-testnet || exit 1
+#   npm ci || exit 1
+#   npx ts-node src/index.ts || exit 1
+# ) || exit 1
 
 docker-compose build || exit 1
-docker-compose up || exit 1
+docker-compose run tests || exit 1
 docker-compose down
