ee8cd9d7f7448176cb779735f83faa0f111e197b
Till <till.friesewinkel@googlemail.com>
2022-10-20 17:12:17 +0100

    Works again locally

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-# FIXME: reactivate
-# (
-#   cd seed-testnet || exit 1
-#   npm ci || exit 1
-#   npx ts-node src/index.ts || exit 1
-# ) || exit 1
+(
+  cd seed-testnet || exit 1
+  npm ci || exit 1
+  npx ts-node src/index.ts || exit 1
+) || exit 1
 
 docker-compose build || exit 1
-docker-compose run tests || exit 1
+docker-compose up -d || exit 1
+echo 'Docker system running'
+
+sleep 10
+while docker ps | grep indexer >/dev/null; do
+  sleep 1
+done
+echo 'Indexer finished'
+
+docker compose exec tests cargo test || exit 1
+
 docker-compose down
