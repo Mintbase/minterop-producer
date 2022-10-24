91f7ee7143243ae352e468314d4bd1a340f162b1
Till <till.friesewinkel@googlemail.com>
2022-10-24 13:50:28 +0100

    Invalidate offers on delisting

--- src/handlers/market_v01/nft_unlist.rs
+++ src/handlers/market_v01/nft_unlist.rs
@@ -25,8 +25,9 @@ async fn handle_nft_unlist_log(
     tx: ReceiptData,
     log: NftUnlistLog,
 ) {
-    future::join(
+    future::join3(
         update_nft_listings(rt.clone(), tx.clone(), log.clone()),
+        invalidate_nft_offers(rt.clone(), tx.clone(), log.clone()),
         insert_nft_activities(rt.clone(), tx.clone(), log.clone()),
     )
     .await;
@@ -60,6 +61,41 @@ async fn update_nft_listings(
     )
     .set(dsl::unlisted_at.eq(tx.timestamp))
     .execute_db(&rt.pg_connection, &tx, "update listing on unlist")
+    .await
+}
+
+async fn invalidate_nft_offers(
+    rt: TxProcessingRuntime,
+    tx: ReceiptData,
+    log: NftUnlistLog,
+) {
+    use minterop_data::schema::nft_offers::dsl;
+    let (nft_contract, token_id, approval_id) =
+        match super::parse_list_id(&log.list_id) {
+            None => {
+                crate::error!(
+                    "Unparseable list ID: {}, ({:?})",
+                    log.list_id,
+                    tx
+                );
+                return;
+            }
+            Some(triple) => triple,
+        };
+
+    diesel::update(
+        nft_offers::table
+            .filter(dsl::nft_contract_id.eq(nft_contract.to_string()))
+            .filter(dsl::token_id.eq(token_id.to_string()))
+            .filter(dsl::market_id.eq(tx.receiver.to_string()))
+            .filter(dsl::approval_id.eq(pg_numeric(approval_id)))
+            .filter(dsl::accepted_at.is_null())
+            .filter(dsl::withdrawn_at.is_null())
+            .filter(dsl::outbid_at.is_null())
+            .filter(dsl::invalidated_at.is_null()),
+    )
+    .set(dsl::invalidated_at.eq(tx.timestamp))
+    .execute_db(&rt.pg_connection, &tx, "invalidate_offer")
     .await
 }
 
