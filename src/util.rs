4b0eafba2943e736a78c984185df88ae2f03805f
Till <till.friesewinkel@googlemail.com>
2024-03-04 10:16:20 +0000

    Add unit test

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,9 @@
-pub(crate) type SafeFractionMap = std::collections::HashMap<
-    mb_sdk::near_sdk::AccountId,
-    mb_sdk::types::nft_core::SafeFraction,
->;
-pub(crate) type U16Map = std::collections::HashMap<String, u16>;
+use mb_sdk::near_sdk::AccountId;
+use mb_sdk::types::nft_core::SafeFraction;
+use std::collections::HashMap;
+
+pub(crate) type SafeFractionMap = HashMap<AccountId, SafeFraction>;
+pub(crate) type U16Map = HashMap<String, u16>;
 
 pub(crate) fn map_fractions_to_u16(
     safe_fraction_map: &SafeFractionMap,
@@ -13,4 +14,37 @@ pub(crate) fn map_fractions_to_u16(
             (account_id.to_string(), safe_fraction.numerator as u16)
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    fn get_safe_fraction_map() -> SafeFractionMap {
+        let mut m = HashMap::new();
+        m.insert(
+            AccountId::from_str("a.near").unwrap(),
+            SafeFraction { numerator: 100 },
+        );
+        m.insert(
+            AccountId::from_str("b.near").unwrap(),
+            SafeFraction { numerator: 200 },
+        );
+        m
+    }
+    fn get_u16_map() -> U16Map {
+        let mut m = HashMap::new();
+        m.insert("a.near".to_string(), 100);
+        m.insert("b.near".to_string(), 200);
+        m
+    }
+
+    #[test]
+    fn test_map_fractions_to_u16() {
+        assert_eq!(
+            map_fractions_to_u16(&get_safe_fraction_map()),
+            get_u16_map()
+        )
+    }
 }
